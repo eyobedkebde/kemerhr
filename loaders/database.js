0439148db7edff4015f0b5176a2c169a3ae9f6cf
eyobedkebde <eyobed@kemercode>
2022-11-16 23:18:28 +0300

    removed some codes

--- loaders/database.js
+++ loaders/database.js
@@ -1,18 +1,12 @@
 const {Client} = require('pg');
 require('dotenv').config();
 
-// const config = require('../config/dbconfig')
 const client = new Client({ 
 
    host: process.env.DB_HOST,
    user: process.env.USER,
    password: process.env.PASSWORD.toString(),
    database: process.env.DATABASE
-    // user: 'postgres',
-    // password: "123456",
-    // host: 'localhost',
-    // port: 5432,
-    // database: 'kemerhrthree'
 });
 
 module.exports = client;
