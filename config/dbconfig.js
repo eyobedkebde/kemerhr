7772bae12f4d7e1d78ea1a282f1cd9bf8fb4ee10
eyobedkebde <eyobed@kemercode>
2022-11-16 23:21:39 +0300

    added port config

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { Pool} = require('pg');
 
 const config = {
-   
+   port:process.env.PORT,
    host: process.env.DB_HOST,
    user: process.env.USER,
    password: process.env.PASSWORD.toString(),
