55b25c39c9af9ee601f86a94568174cce3eb9578
eyobedkebde <eyobed@kemercode>
2022-11-16 23:20:38 +0300

    added port config

--- loaders/database.js
+++ loaders/database.js
@@ -2,7 +2,7 @@ const {Client} = require('pg');
 require('dotenv').config();
 
 const client = new Client({ 
-
+   port:process.env.PORT,
    host: process.env.DB_HOST,
    user: process.env.USER,
    password: process.env.PASSWORD.toString(),
